07fade1c09537e356d7eda153d52eaf371d86296
Deison Ribeiro <deisonribeiro14@gmail.com>
2024-08-12 23:41:57 -0300

    fixing requests with root mode

--- server.js
+++ server.js
@@ -63,7 +63,10 @@ app.get("/api/render", async (req, res) => {
 });
 
 const processRequest = async (url) => {
-  const browser = await puppeteer.launch();
+  // Lançar o Puppeteer com as opções --no-sandbox e --disable-setuid-sandbox
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
   const content = await page.content();
